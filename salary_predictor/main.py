6301cb77d887cb1c30a1ca96c1fe249c70e25894
Kasaksaxena <kasaksaxena6@gmail.com>
2025-07-15 20:35:01 +0530

    Bonus Regularization(lasso and ridge)

--- salary_predictor/main.py
+++ salary_predictor/main.py
@@ -1,6 +1,6 @@
 from src.loader import load_data
 from src.preprocess import preprocess_data
-from src.odel import train_linear_model, train_polynomial_model
+from src.odel import train_linear_model, train_polynomial_model,train_lasso_model,train_ridge_model
 from sklearn.model_selection import train_test_split
 from src.evaluate import evaluate_model
 if __name__=="__main__":
@@ -16,11 +16,22 @@ if __name__=="__main__":
     print("\n Training Polynomial Regression (degree=2) \n")
     poly_model=train_polynomial_model(X_train,y_train,degree=2 )
     
+    print("\n Training ridge  \n")
+    ridge_model=train_ridge_model(X_train,y_train,alpha=1.0)
+    
+    print("\n Training Lasso  \n")
+    lasso_model=train_lasso_model(X_train,y_train,alpha=0.1)
+   
+    
     print("\n Model Trained Successfully!")
     
     # evaluate
     evaluate_model(linear_model,X_test,y_test,title="Linear Regression")
     evaluate_model(poly_model,X_test,y_test,title="Polynomial Regression(Degree 2)")
+    evaluate_model(ridge_model,X_test,y_test,title="Ridge Regression")
+    evaluate_model(lasso_model,X_test,y_test,title="Lasso Regression")
+    
+    
     
     # print("\n Processing complete \n")
     # print("features shape",X.shape)
--- salary_predictor/src/odel.py
+++ salary_predictor/src/odel.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso,Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 
@@ -11,3 +11,13 @@ def train_polynomial_model(X,y,degree=2):
     poly_model=make_pipeline(PolynomialFeatures(degree),LinearRegression())
     poly_model.fit(X,y)
     return poly_model
+
+def train_ridge_model(X,y,alpha):
+    ridge_model=Ridge(alpha=alpha)
+    ridge_model.fit(X,y)
+    return ridge_model
+
+def train_lasso_model(X,y,alpha):
+    lasso_model=Lasso(alpha=alpha)
+    lasso_model.fit(X,y)
+    return lasso_model
